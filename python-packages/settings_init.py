199e74fb555e698e8452d55f9805f1c362afa83d
SUPERNOOB20 <andycia802@gmail.com>
2025-05-07 18:14:31 -0300

    v0.0.6.1-alpha (I forgot to do some casting it seems ehehehe "^^)

--- python-packages/settings_init.py
+++ python-packages/settings_init.py
@@ -6,6 +6,9 @@
 # to the world map when it opens up :)
 
 import settings
+import os
+current_dir = os.getcwd()
+
 
 def initialize_region_progress():
 
@@ -16,11 +19,13 @@ def initialize_region_progress():
 
 def load_from_nexus() -> list[bool]:
 
-    nexus_file = open("nexus.txt", "r")
-    nexus_file = nexus_file.readlines()
+    global current_dir
+    nexus_textfile = open(f"{current_dir}/python-packages/nexus.txt", "r")
+    # nexus_textfile = open("/python-packages/nexus.txt", "r")
+    nexus_file = nexus_textfile.readlines()
 
     region_progress = []
-    for i in range(2, 14):
+    for i in range(1, 16):
         region_progress.append(nexus_file[i])
 
     return region_progress
@@ -29,32 +34,41 @@ def load_from_nexus() -> list[bool]:
 def save_to_settings_file(region_progress):
     # settings_file = open("settings.py", "w")
 
-    settings.has_completed_red_region = region_progress[0]
-    settings.has_completed_orange_region = region_progress[1]
-    settings.has_completed_yellow_region = region_progress[2]
-    settings.has_completed_green_region = region_progress[3]
-    settings.has_completed_blue_region = region_progress[4]
-    settings.has_completed_purple_region = region_progress[5]
+    settings.has_completed_red_region = bool(region_progress[0])
+    settings.has_completed_orange_region = bool(region_progress[1])
+    settings.has_completed_yellow_region = bool(region_progress[2])
+    settings.has_completed_green_region = bool(region_progress[3])
+    settings.has_completed_blue_region = bool(region_progress[4])
+    settings.has_completed_purple_region = bool(region_progress[5])
 
-    should_black_region_be_available(region_progress[0], region_progress[1], region_progress[2], region_progress[3], region_progress[4], region_progress[5])
+    should_black_region_be_available(settings.has_completed_red_region, settings.has_completed_orange_region, settings.has_completed_yellow_region, settings.has_completed_green_region, settings.has_completed_blue_region, settings.has_completed_purple_region)
 
-    settings.has_completed_black_region = region_progress[6]
+    settings.has_completed_black_region = bool(region_progress[6])
 
-    settings.has_completed_red_postgame = region_progress[7]
-    settings.has_completed_orange_postgame = region_progress[8]
-    settings.has_completed_yellow_postgame = region_progress[9]
-    settings.has_completed_green_postgame = region_progress[10]
-    settings.has_completed_blue_postgame = region_progress[11]
-    settings.has_completed_purple_postgame = region_progress[12]
+    settings.has_completed_red_postgame = bool(region_progress[7])
+    settings.has_completed_orange_postgame = bool(region_progress[8])
+    settings.has_completed_yellow_postgame = bool(region_progress[9])
+    settings.has_completed_green_postgame = bool(region_progress[10])
+    settings.has_completed_blue_postgame = bool(region_progress[11])
+    settings.has_completed_purple_postgame = bool(region_progress[12])
 
-    settings.sfx_volume = region_progress[13]
-    settings.bgm_volume = region_progress[14]
+    settings.sfx_volume = float(region_progress[13])
+    settings.bgm_volume = float(region_progress[14])
 
     return
 
 
 
 def should_black_region_be_available(red, orange, yellow, green, blue, purple):
+    
+    # red = bool(red)
+    # orange = bool(orange)
+    # yellow = bool(yellow)
+    # green = bool(green)
+    # blue = bool(blue)
+    # purple = bool(purple)
+
+
     if (red & orange & yellow & green & blue & purple):
         settings.has_completed_black_region = True
     return
